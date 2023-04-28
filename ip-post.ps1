f8737c34c22305fd216daf99e4fe7454141a6951
ds2600 <bill@ishiganto.com>
2023-04-28 15:24:28 -0400

    Cleaned up and added log deletion

--- ip-post.ps1
+++ ip-post.ps1
@@ -1,4 +1,8 @@
-﻿Get-Date >> C:\ip-post\ip-post.log
+﻿$LOCAL_LOG = "C:\ip-post\ip-post.log"
+$LOG_SIZE = (Get-Item $LOCAL_LOG).length/1MB
+If ($LOG_SIZE -gt 10) { Remove-Item $LOCAL_LOG }
+
+Get-Date >> C:\ip-post\ip-post.log
 Get-Content "C:\ip-post\settings.ini" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } } 
 $UUID = (wmic csproduct get uuid)[2]
 
@@ -14,7 +18,7 @@ $IP_INFO = Invoke-RestMethod -Uri ('http://ipinfo.io/'+(Invoke-WebRequest -uri "
 $IP_INFO | Add-Member $ADDITIONAL_PAR
 $BODY = $IP_INFO | ConvertTo-Json
 
-echo "Sending request to: " + $h.Get_Item("API_URI") >> C:\ip-post\ip-post.log
+Write-Output "Sending request to: " + $h.Get_Item("API_URI") >> C:\ip-post\ip-post.log
 $RESP = Invoke-WebRequest -Method Post -Uri $h.Get_Item("API_URI") -Body $BODY -UseBasicParsing >> C:\ip-post\ip-post.log
 $RESP >> C:\ip-post\ip-post.log
-echo "==============="  >> C:\ip-post\ip-post.log
+Write-Output "==============="  >> C:\ip-post\ip-post.log
